729f871bf5e17565bf07a4b2617b9787c6b55979
marta <rulex12@gmail.com>
2018-02-28 16:04:22 +0100

    Funciona API login

--- api/controllers/empresaController.js
+++ api/controllers/empresaController.js
@@ -15,17 +15,14 @@ exports.create_a_company = function (req, res) {
 
 exports.login_emp = function (req, res) {
   console.log(req.body);
-  console.log(req.body.email_emp);
-  console.log(req.body.password_emp);
 
-  
-  Empresas.find({ email: req.body.email_emp, password: req.body.password_emp }, (err, empArr) => {
+  Empresas.find({ email_emp: req.body.email_emp, password_emp: req.body.password_emp }, (err, empArr) => {
     console.log('respuesta Login:',err, empArr);
     if (empArr.length != 1) {
       res.status(401);
       res.send("Not found");
     }
-    else if (empArr[0]['email'] != req.body.email_emp || empArr[0]['password'] != req.body.password_emp) {
+    else if (empArr[0]['email_emp'] != req.body.email_emp || empArr[0]['password_emp'] != req.body.password_emp) {
       res.status(401);
       res.send("Email o password incorrectos");
     }
